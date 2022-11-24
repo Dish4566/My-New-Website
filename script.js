61c83cf507a0c6348c0aa10126d8adcd6793497a
Dish4566 <rsondhi3132@gmail.com>
2022-11-24 14:32:40 -0600

    ooooooo

--- script.js
+++
@@ -1,9 +0,0 @@
-let tab = document.querySelector('.chapters');
-let buttons = document.querySelector('.sideButtons')
-let button = document.querySelector('.showButton');
-let moveBy = 10;
-
-button.addEventListener('click', () => {
-    tab.style.width = '0vh';
-    buttons.style.width = '0vh';
-});
